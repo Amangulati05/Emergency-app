fc69637b1ee7f0395da9b81a7695cb383d3b576b
Amangulati05 <131166446+Amangulati05@users.noreply.github.com>
2024-04-13 22:26:42 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -18,56 +18,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyhomePage extends StatefulWidget {
+  const MyhomePage({super.key});
+
+  @override
+  State<MyhomePage> createState() => _MyhomePageState();
+}
+
+class _MyhomePageState extends State<MyhomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Container(
+          child: const Text('Select your emergeency'),
+          height: 100,
+          width: 100,
+        ),
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Container(child: Center(child: Text('Select your Emergency '))),
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: Column(
+        appBar: AppBar(),
+        body: Center(
+            child: Column(children: [
+              Container(
+                child:Center(child: Text('Selct your emergency')),
+                color: Colors.red,
 
-
-          children: [
-
-            Expanded(
-              child: Container(
-                      height: 200,
-                      width: 200,
-                      child: Image(
-                        image: AssetImage('assets/DiabeteszImage.jpg'),
-
-                      )
-                    ),
-            ),
-
-
-            SizedBox(
-              width: 100,
-              height: 100,
-
-
-
-                  ),
-
-
-            SizedBox(height: 20),
-            SizedBox(
-              width: 200,
-              height: 200,
-              child: Container(
-
-                child: Center(
-                  child: Image.asset('assets/Heart attack 2.jpg')
-                  ),
-                ),
+                height: 50,
+                width: 500,
               ),
+          SizedBox(
+            height: 30,
+            width: 30,
+          ),
 
-          ],
-        ),
-      ),
-    );
+          Expanded(
+            child: Center(
+              child: Container(
+                  height: 200,
+                  width: 180,
+                  child: Column(
+                    children: [
+
+
+                      Image(
+                        image: AssetImage('assets/Diabetes Image.jpg'),
+
+
+
+                      ),
+                    ],
+                  )),
+
+            ),
+          ),
+          SizedBox(
+            width: 100,
+            height: 100,
+          ),
+
+
+          SizedBox(height: 20),
+          SizedBox(
+            width: 200,
+            height: 200,
+            child: Container(
+              child: Center(child: Image.asset('assets/Heart attack 2.jpg')),
+            ),
+          ),
+        ])));
   }
 }
